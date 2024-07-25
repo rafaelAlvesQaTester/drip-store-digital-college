339796663a04bd4126b99a15bd1eca2e1aa760a9
Marcos Rafael Alves <rafaelalves1803tester@gmail.com>
2024-07-24 21:14:36 -0300

    Estruturacao dos conteudos parte 2

---
+++ drip-store-digital-college_project/src/assets/pages/ProductListingPage.jsx
@@ -0,0 +1,9 @@
+const ProducListingPage = () => {
+    return ( 
+        <>
+            
+        </>
+     );
+}
+ 
+export default ProducListingPage;
---
+++ drip-store-digital-college_project/src/components/ProductOptions.jsx
@@ -0,0 +1,9 @@
+const ProductOptions = () => {
+    return ( 
+        <>
+
+        </>
+     );
+}
+ 
+export default ProductOptions;
